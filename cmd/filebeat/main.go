a8966a3df00cccce65d20063adef19b8c9a0cfd9
Link <Rabbit20110410@gmail.com>
2022-01-20 09:41:09 +0800

    初始化工程

---
+++ cmd/filebeat/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"github.com/elastic/beats/v7/filebeat/cmd"
+	inputs "github.com/elastic/beats/v7/filebeat/input/default-inputs"
+)
+
+func main() {
+	if err := cmd.Filebeat(inputs.Init, cmd.FilebeatSettings()).Execute(); err != nil {
+		os.Exit(1)
+	}
+}
